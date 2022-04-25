74f545396401f89bcabe60fe534604f962da34a1
wamiya <32713167+MYWang23@users.noreply.github.com>
2022-04-25 16:04:08 +0100

    MainMenu

---
+++ Assets/Scripts/MainMeun.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMeun : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void Option()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
